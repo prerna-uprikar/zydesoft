05e750c5bb9a1bee36d127a76510934c81550cca
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2024-03-03 00:35:39 +0530

    hero section added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,20 +8,23 @@ function Hero() {
             className={styles.lotus}
             src="https://images01.nicepagecdn.com/c461c07a441a5d220e8feb1a/352b95a02a155f489ec4e725/bn.png"
           />
-          <h1  className={styles.lotusHeading}>Welcome!</h1>
-          <p  className={styles.lotusPara}>
+          <h1 className={styles.lotusHeading}>Welcome!</h1>
+          <p className={styles.lotusPara}>
             Every human being should have at least a simple spiritual <br />{" "}
             process in their lives. If they have that, they will live and <br />{" "}
             die well.
           </p>
-          <p  className={styles.lotusPara}>
+          <p className={styles.lotusPara}>
             Image from <a>freepik</a>
           </p>
-          <button  className={styles.lotusButton}>READ MORE</button>
+          <button className={styles.lotusButton}>READ MORE</button>
         </div>
 
         <div>
-            <img className={styles.yoga} src="https://assets.nicepagecdn.com/d2cc3eaa/3629861/images/65556.jpg"/>
+          <img
+            className={styles.yoga}
+            src="https://images01.nicepagecdn.com/c461c07a441a5d220e8feb1a/397aec59aed553968551ef89/tytyy.jpg"
+          />
         </div>
       </div>
     </>
